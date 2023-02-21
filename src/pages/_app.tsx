d082d197a1b3a4b7c43e2638696c2a7bb292b141
hjcasayas <henlyjade.casayas@gmail.com>
2023-02-21 23:16:44 +0800

    feat: added tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Dev Challenges - 404 Not Found</title>
+        <meta
+          name="description"
+          content="A solution for DevChallenges.io 404 Not Found challenge."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/devchallenges.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
